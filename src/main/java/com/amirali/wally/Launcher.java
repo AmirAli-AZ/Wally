fee622604764b9dea6a9a34509017526411c9ca8
AmirAli <84965706+AmirAli-AZ@users.noreply.github.com>
2023-02-13 23:09:00 +0330

    update: modular project

--- src/main/java/com/amirali/wally/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.amirali.wally;
-
-public class Launcher {
-
-    public static void main(String[] args) {
-        App.main(args);
-    }
-}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.amirali.wally {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires mongo.java.driver;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.fontawesome;
+
+    opens com.amirali.wally.ui.controllers to javafx.fxml;
+    exports com.amirali.wally;
+}
